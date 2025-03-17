27a7e58b43bcd3ed00ae423de694a9d7d48b76bd
Jonas Magalhães <info.jonas@gmail.com>
2025-03-17 10:43:01 -0300

    add model and migration to 'produto'

---
+++ laravel-app/app/Models/Produto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'category_id', 'brand_id'];
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function marca(): BelongsTo
+    {
+        return $this->belongsTo(Marca::class);
+    }
+}
